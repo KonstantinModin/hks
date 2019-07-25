1d4bfa83ef7bbe72abb418893d0c31f8d5520eaa
Konstantin <konstantin.v.modin@gmail.com>
2019-07-25 05:59:00 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './components/Todo';
+import Auth from './components/Auth';
+import Header from './components/Header';
+import AuthContext from './auth-context';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [state, setState] = useState({list: false, auth: true});
+    const [authStatus, setAuthStatus] = useState(false);
+
+    console.log(AuthContext);
+
+    const login = () => {
+        setAuthStatus(!authStatus);
+    }
+    
     return (
         <div className="App">
-            <h1>Another todo App!</h1>
-            <Todo />
+            <AuthContext.Provider value={{ status: authStatus, login }}>
+                <h1>Another todo App!</h1>
+                <Header 
+                    onLoadAuth={() => setState({list: false, auth: true})} 
+                    onLoadTodos={() => setState({list: true, auth: false})}
+                    />
+                <hr />
+                {state.list && <Todo />}
+                {state.auth && <Auth />}
+            </AuthContext.Provider>
         </div>
     );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Todo.css';
 
 const Todo = () => {
 
     const [inputValue, setInputValue] = useState('');
     const [todoItems, setTodoItems] = useState([]);
-    
-    const inputHandler = ({target: {value}}) => {
-        // console.log(value);
-        setInputValue(value);
-    }
 
-    const buttonHandler = (event) => {
-        // console.log('button', event);
+    useEffect(() => {
+        axios.get('https://hks-testing.firebaseio.com/todos.json')
+            .then(response => console.log(response))
+            .catch(e => console.log(e));
+        return () => console.log('cleanup!');
+    }, [inputValue]);
+
+    
+    // useEffect(() => {
+    //     document.addEventListener('mousemove', mouseMoveHandler);
+    //     return () => document.removeEventListener('mousemove', mouseMoveHandler);
+    // });
+    
+    // const mouseMoveHandler = ({ clientX, clientY }) => console.log(clientX, clientY);
+
+    const inputHandler = ({target: {value}}) => setInputValue(prevState => {
+        console.log('prevState', prevState);
+        return value;
+    });
+
+    const buttonHandler = () => {        
         setTodoItems([...todoItems, inputValue]);
         setInputValue('');
-    }
+    };
+
+    const postTodoHandler = () => {
+        axios.post('https://hks-testing.firebaseio.com/todos.json', todoItems)
+            .then(response => console.log('response', response))
+            .catch(error => console.log('error', error));
+        console.log('post todo Handler');
+    };
     
     return (
         <div className="Todo">
@@ -24,6 +46,7 @@ const Todo = () => {
             <ul>
                 {todoItems.map((i) => <li key={Math.random()}>{i}</li>)}
             </ul>
+            <button onClick={postTodoHandler} type="button">Post Todos</button>
         </div>
     )
 }
