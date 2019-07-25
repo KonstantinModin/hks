2f11bd67af0d0dbe4031518bea4c7b0cdfc9d196
Konstantin <konstantin.v.modin@gmail.com>
2019-07-25 07:17:36 +0200

    updates

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import './Todo.css';
 
 const Todo = () => {
 
     const [inputValue, setInputValue] = useState('');
-    const [todoItems, setTodoItems] = useState([]);
+    // const [todoItems, setTodoItems] = useState([]);   
+
+    const todoListReducer = (state, { type, payload }) => {
+        switch (type) {
+            case 'ADD': return [...state, payload];
+            case 'SET': return [...state, ...payload];
+            case 'REMOVE': return state.filter(i => i !== payload);
+            default: return state;
+        }
+    };
+
+    const [ todoItems, dispatch ] = useReducer(todoListReducer, []);
+
+    const plusMinusReducer = (state, {type}) => {
+        switch (type) {
+            case '+': return state + 1;
+            case '-': return state - 1;
+            default: return state;
+        };
+    };
+
+    const [ counter, dispatchCounter] = useReducer(plusMinusReducer, 0);
+
+
 
     useEffect(() => {
         axios.get('https://hks-testing.firebaseio.com/todos.json')
-            .then(response => console.log(response))
+            .then(response => {
+                const arr = Object.entries(response.data).reduce((a, b) => a.concat(b[1]), []);
+                console.log('todos from server:', arr);
+                dispatch({type: 'SET', payload: arr});
+            })
             .catch(e => console.log(e));
         return () => console.log('cleanup!');
-    }, [inputValue]);
-
+    }, []);
     
     // useEffect(() => {
     //     document.addEventListener('mousemove', mouseMoveHandler);
@@ -22,15 +48,24 @@ const Todo = () => {
     
     // const mouseMoveHandler = ({ clientX, clientY }) => console.log(clientX, clientY);
 
-    const inputHandler = ({target: {value}}) => setInputValue(prevState => {
-        console.log('prevState', prevState);
-        return value;
-    });
-
+    const inputHandler = ({target: {value}}) => {
+        setInputValue(prevState => {
+            // console.log('prevState', prevState);
+            return value;
+        })
+    };
+        
     const buttonHandler = () => {        
-        setTodoItems([...todoItems, inputValue]);
+        // setTodoItems([...todoItems, inputValue]);
+        dispatch({type: 'ADD', payload: inputValue});            
         setInputValue('');
     };
+
+    const removeItemHandler = (e) => {
+        // e.persist();
+        console.log(e.target.textContent);
+        dispatch({type: 'REMOVE', payload: e.target.textContent});
+    }
 
     const postTodoHandler = () => {
         axios.post('https://hks-testing.firebaseio.com/todos.json', todoItems)
@@ -44,9 +79,13 @@ const Todo = () => {
             <input onChange={inputHandler} type="text" placeholder="Enter your task" value={inputValue}/>
             <button onClick={buttonHandler} type="button">Add</button>
             <ul>
-                {todoItems.map((i) => <li key={Math.random()}>{i}</li>)}
+                {todoItems.map((i) => <li onClick={removeItemHandler} key={Math.random()}>{i}</li>)}
             </ul>
             <button onClick={postTodoHandler} type="button">Post Todos</button>
+            <hr/>
+            <h2>Our counter: {counter}</h2>
+            <button onClick={() => dispatchCounter({type: '+'})}>+</button>
+            <button onClick={() => dispatchCounter({type: '-'})}>-</button>
         </div>
     )
 }
